f7b303de1ece6e293a4c50c9c62014a919657c88
Leonardo Souza <leogsouza@gmail.com>
2023-12-26 23:37:38 +0000

    Create endpoint to process video

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,13 +1,34 @@
 import express, { Request, Response } from 'express'
+import ffmpeg from 'fluent-ffmpeg'
 
 const app = express()
-const port = 3000;
+app.use(express.json())
+
+app.post('/process-video', (req: Request, res: Response) => {
+  const inputFilePath = req.body.inputFilePath
+  const outputFilePath = req.body.outputFilePath
+
+  if (!inputFilePath) {
+    return res.status(400).send("Bad Request: Missing input file path")
+  } else if (!outputFilePath) {
+    return res.status(400).send("Bad Request: Missing output file path")
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions('-vf', 'scale=-1:360') // 360p
+    .on('end', () => {
+      res.status(200).send('Processing finished successfully.')
+    })
+    .on('error', (err) => {
+      console.log(`An error occurred: ${err.message}`)
+      res.status(500).send(`Internal Server Error: ${err.message}`)
+    })
+    .save(outputFilePath)
 
 
-app.get('/', (req:Request, res:Response) => {
-  res.send("Video processing is on")
 })
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Video processing listening at http://localhost:${port}`)
 })
